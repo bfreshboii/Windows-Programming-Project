9810efc41bdc496a1aeed97c9e91410c19eb27d2
Mark Jamieson-Simmons <W0450992@nscc.ca>
2023-04-11 20:14:28 -0300

    created models

---
+++ Movie_Application_Project/Models/Generated/Name.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PROG2500_AF_IMDB_Project.Models;
+
+public partial class Name
+{
+    public string NameId { get; set; } = null!;
+
+    public string? PrimaryName { get; set; }
+
+    public int? BirthYear { get; set; }
+
+    public int? DeathYear { get; set; }
+
+    public string? PrimaryProfession { get; set; }
+
+    public virtual ICollection<Principal> Principals { get; } = new List<Principal>();
+
+    public virtual ICollection<Title> Titles { get; } = new List<Title>();
+
+    public virtual ICollection<Title> Titles1 { get; } = new List<Title>();
+
+    public virtual ICollection<Title> TitlesNavigation { get; } = new List<Title>();
+}
