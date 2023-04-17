109b55acd5fbde4edbc688de605e0d33e5dcbd2c
Chichii-Marvey <onyekwere_marvelous@yahoo.com>
2023-04-17 08:59:23 -0300

    Try

--- Movie_Application_Project/Pages/ActorsPage .xaml.cs
+++ Movie_Application_Project/Pages/ActorsPage .xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PROG2500_AF_IMDB.Project.Data;
+using PROG2500_AF_IMDB.Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,33 @@ namespace Movie_Application_Project.Pages
     /// </summary>
     public partial class ActorsPage : Page
     {
+        private readonly ImdbProjectContext _context = new ImdbProjectContext();
+        private CollectionViewSource actorsViewSource;
         public ActorsPage()
         {
             InitializeComponent();
+
+            actorsViewSource = (CollectionViewSource)FindResource(nameof(actorsViewSource));
+            actorsViewSource.Source = _context.Names.Local.ToObservableCollection();
+
+
+        }
+        public void btnSearchClicked(object sender, RoutedEventArgs e)
+        {
+            Name name = _context.Names.Find(100);
+            _context.Entry(name)
+                    .Collection(p => p.Principals)
+                    .Query()
+                    .Where(p => p.JobCategory.Contains(textSearch.Text))
+                    .Load();
+
+
+            var query =
+                from p in _context.Principals
+                select p;
+
+            actorsViewSource.Source = query.ToList();
+
         }
     }
 }
