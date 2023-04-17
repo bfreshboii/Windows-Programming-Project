9ad8024ff0ca257df4628a89ac24e0db9219b9a2
Teixeira, Gabriel <w0452219@nscc.ca>
2023-04-16 23:09:07 -0300

    Some more updates

--- Movie_Application_Project/Pages/DirectorsPage.xaml.cs
+++ Movie_Application_Project/Pages/DirectorsPage.xaml.cs
@@ -38,50 +38,14 @@ namespace Movie_Application_Project.Pages
 
         private void DirectrosSearcButton_Click(object sender, RoutedEventArgs e)
         {
-            // _context.Names.Load();
-            //var query = _context.Names.Where(name => name.PrimaryName.Contains(txtSearch.Text)).ToList();
-            //directorsListView.ItemsSource = query;
-
-            //Name name = _context.Names.FirstOrDefault();
-            //Genre genre = _context.Genres.Find(1);
-
-            //_context.Entry(name)
-            //_context.Entry(genre)
-
-
-            //.Collection(p => p.Principals)
-            //.Query()
-            //.Where(t => t.JobCategory.Contains("director"))
-            //.Select (p => p.Name)
-            //.Load();
-
-
-
 
 
             var query = (from nam in _context.Names.Take(100)
                          join pri in _context.Principals on nam.NameId equals pri.NameId
                          where nam.PrimaryName.Contains(txtSearch.Text) && pri.JobCategory.Contains("director")
+                         orderby nam ascending
                          select nam).Distinct();
 
-
-
-            //var query =
-            //       from title in _context.Titles
-            //        where title.PrimaryTitle.Contains(textsearch.Text) && (title.Names.Any(g => g.PrimaryName.Contains(selectedgenre ?? string.Empty)) || title.Names1.Any(g => g.PrimaryName.Contains(selectedgenre ?? string.Empty)))
-            //        orderby title.PrimaryTitle
-            //        select title;
-            //
-
-
-            // listTitlesSearchResults.Items.Clear();
-            // foreach (var title in query.Take(500))
-            //  {
-            //     listTitlesSearchResults.Items.Add(title);
-            //  }
-
-
-            //var primaryProfessions = _context.Names.Select(n => n.PrimaryProfession).Distinct().ToList();
             directorsViewSource.Source = query.ToList();
             directorsListView.ItemsSource = query.ToList();
         }
--- Movie_Application_Project/obj/Debug/net7.0-windows/Pages/ActorsPage .g.i.cs
+++ Movie_Application_Project/obj/Debug/net7.0-windows/Pages/ActorsPage .g.i.cs
@@ -53,7 +53,7 @@ namespace Movie_Application_Project.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Movie_Application_Project;V1.0.0.0;component/pages/actorspage%20.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Movie_Application_Project;component/pages/actorspage%20.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\ActorsPage .xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
